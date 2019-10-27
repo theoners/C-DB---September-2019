6fccd54e9e7b5bef126551933cd020c576fb757f
theoners <theonebgrs@gmail.com>
2019-10-27 23:45:18 +0100

    added task

--- Entity Framework Core - October 2019/Introduction to DB Apps-Exercises/1. Initial Setup/SQLCmd.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _1._Initial_Setup
-{
-    public class SQLCmd
-    {
-        public static string createDB = $"Create Database {0}";
-        public static string createTable = "signup";
-        public static string insertData = "signup";
-    }
-}
---
+++ Entity Framework Core - October 2019/Introduction to DB Apps-Exercises/6. Remove Villain/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _6._Remove_Villain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var villainId = Console.ReadLine();
+            var connectionString = new SqlConnection(@"Server=ASUS\SQLEXPRESS;Database=MinionsDB;Integrated security=true;");
+
+            connectionString.Open();
+
+            using (connectionString)
+            {
+                var getVillainCommand = new SqlCommand($"SELECT Name FROM Villains WHERE Id = {villainId}",connectionString);
+                var villainName = getVillainCommand.ExecuteScalar();
+                if (villainName!=null)
+                {
+                    var deleteFromMinionsVillainsTable = new SqlCommand($"DELETE FROM MinionsVillains WHERE VillainId = {villainId}", connectionString);
+                    var deleteVillainCommand = new SqlCommand($@" DELETE FROM Villains WHERE Id = {villainId}", connectionString);
+                    var minionsCount= deleteFromMinionsVillainsTable.ExecuteNonQuery();
+                    Console.WriteLine($"{villainName} was deleted.");
+                    deleteVillainCommand.ExecuteNonQuery();
+                    Console.WriteLine($"{minionsCount} minions were released.");
+                }
+                else
+                {
+                    Console.WriteLine("No such villain was found.");
+                }
+               
+
+               
+            }
+
+        }
+    }
+}
