de2d113021a27535a29b25133c83d8726b5b2975
theoners <theonebgrs@gmail.com>
2019-10-28 21:27:06 +0100

    added ORM Fundamentals-Exercises

---
+++ Entity Framework Core - October 2019/ORM Fundamentals-Exercises/MiniORMAPPS/Data/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniORMAPPS.Data.Entities
+{
+
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class Department
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public ICollection<Employee> Employees { get; }
+        }
+}
+
---
+++ Entity Framework Core - October 2019/ORM Fundamentals-Exercises/MiniORMAPPS/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace MiniORMAPPS
+{
+    using System.Linq;
+    using Data.Entities;
+    class StartUp
+    {
+        public static void Main()
+        {
+            var connectionString = @"Server=ASUS\SQLEXPRESS;Database=MiniORM;Integrated security=true;";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
